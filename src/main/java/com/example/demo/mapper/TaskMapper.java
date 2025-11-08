9fb88321816cfc54c4c5227b3bdd57b029a82ee3
Juan Gomes <juanhgomes@gmail.com>
2025-11-07 21:01:52 -0300

    Finanalização mapper

--- src/main/java/com/example/demo/mapper/TaskMapper.java
+++ src/main/java/com/example/demo/mapper/TaskMapper.java
@@ -3,13 +3,15 @@ package com.example.demo.mapper;
 import com.example.demo.dto.request.TaskRequest;
 import com.example.demo.dto.response.TaskResponse;
 import com.example.demo.entity.Task;
+import com.example.demo.enums.Status;
 
 public class TaskMapper {
 
     public static Task map(final TaskRequest request){
         return Task.builder()
                 .title(request.title())
-                .description(request.status())
+                .description(request.description())
+                .status(Status.PENDENTE)
                 .id(request.userId())
                 .build();
     }
